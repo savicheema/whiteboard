602d75ba830bc59414d4e6455ebd50fe4359fd18
rainbow1223 <greatwisdom1223@gmail.com>
2023-07-30 01:04:17 +0900

    add canvas and tool panel buttons

---
+++ src/components/ToolPanel.js
@@ -0,0 +1,14 @@
+const ToolPanel = () => {
+    return(
+        <div>
+            <button>Line</button>
+            <button>Rectangle</button>
+            <button>Triangle</button>
+            <button>Color</button>
+            <button>Eraser</button>
+            <button>Text</button>
+        </div>
+    )
+}
+
+export default ToolPanel; 
